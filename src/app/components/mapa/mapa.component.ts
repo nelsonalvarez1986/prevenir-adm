4a5183eeebce1dcff4419f80136f6092ba06325b
Nelson Alvarez <nelsonalvarez.bsso@gmail.com>
2020-09-22 15:59:45 -0300

    Metodo que entrega los positivos por barrios en el componente mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from "@angular/core";
 //import * as Leaflet from 'leaflet';
-import * as L from 'leaflet';
-import { antPath } from 'leaflet-ant-path';
-import { ShapeService } from '../../services/shape.service';
-import { Cantidades } from '../../interfaces/cantidades';
-import { style } from '@angular/animations';
-import { FirebaseService } from '../../services/firebase.service';
-
+import * as L from "leaflet";
+import { antPath } from "leaflet-ant-path";
+import { ShapeService } from "../../services/shape.service";
+import { Cantidades } from "../../interfaces/cantidades";
+import { style } from "@angular/animations";
+import { FirebaseService } from "../../services/firebase.service";
 
 @Component({
-  selector: 'app-mapa',
-  templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.scss']
+  selector: "app-mapa",
+  templateUrl: "./mapa.component.html",
+  styleUrls: ["./mapa.component.scss"],
 })
 export class MapaComponent implements AfterViewInit {
-
   @Input() cantidades: Cantidades;
 
   private map;
 
   private states;
 
-  public barrio: String = '';
+  public barrio: String = "";
   public data: any;
-  public cargando:boolean = true;
+  public cargando: boolean = true;
 
-  
-  constructor( private shapeService: ShapeService, private firebase: FirebaseService ) { }
+  constructor(
+    private shapeService: ShapeService,
+    private firebase: FirebaseService
+  ) {}
 
   ngAfterViewInit(): void {
-    this.initMap()
+    this.initMap();
 
-    this.shapeService.getStateShapes().subscribe(states => {
+    this.shapeService.getStateShapes().subscribe((states) => {
       this.states = states;
       this.highlightFeature;
       this.resetFeature;
@@ -40,65 +40,88 @@ export class MapaComponent implements AfterViewInit {
   }
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ -34.9007213, -57.8646547 ],
-      zoom: 12
+    this.map = L.map("map", {
+      center: [-34.9007213, -57.8646547],
+      zoom: 12,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
 
-    
-
-  tiles.addTo(this.map);
-  tiles.addTo(this.map);
-
+    tiles.addTo(this.map);
+    tiles.addTo(this.map);
   }
 
-  private initStatesLayer() {
+  private async initStatesLayer() {
+    // Lo saque a otro metodo aparte.
+    const positivos = await this.positivosDeLosBarrios().then((resp) => resp);
+    console.log(positivos); // borrar al terminar
+
     const stateLayer = L.geoJSON(this.states, {
       style: (feature) => ({
         weight: 3,
         opacity: 0.5,
-        color: '#008f68',
+        color: "#008f68",
         fillOpacity: 0.8,
-        fillColor: '#6DB65B'
+        fillColor: "#6DB65B",
       }),
-      onEachFeature: (feature, layer) => (
+      onEachFeature: (feature, layer) =>
         layer.on({
-          mouseover: (e) => (this.highlightFeature(e, feature.properties['Name'])),
-          mouseout: (e) => (this.resetFeature(e)),
-        })
-      )
+          mouseover: (e) =>
+            this.highlightFeature(e, feature.properties["Name"]),
+          mouseout: (e) => this.resetFeature(e),
+        }),
     });
 
     this.map.addLayer(stateLayer);
   }
 
-  
-  private async highlightFeature(e,barrio)  {
+  private async highlightFeature(e, barrio) {
     this.cargando = true;
     const layer = e.target;
     this.barrio = barrio;
-    this.data = await this.firebase.estadisticasPorBarrio(barrio).then(resp => resp);
+    this.data = await this.firebase
+      .estadisticasPorBarrio(barrio)
+      .then((resp) => resp);
     this.cargando = false;
   }
 
-  private resetFeature(e)  {
+  private resetFeature(e) {
     const layer = e.target;
     layer.setStyle({
       weight: 3,
       opacity: 0.5,
-      color: '#19F3BF',
+      color: "#19F3BF",
       fillOpacity: 0.8,
-      fillColor: '#F9BE16'
+      fillColor: "#F9BE16",
     });
-    this.data = undefined
-    this.barrio = undefined
-
+    this.data = undefined;
+    this.barrio = undefined;
   }
 
   
+  // Metodo que devuelve array con los positivos de cada barrio
+  async positivosDeLosBarrios() {
+    const todasLasPersonas = await this.firebase
+      .getAllPersonas()               
+      .then((resp) => resp);
+
+    const valoresPorBarrio = await this.firebase
+      .totalesBarrio(todasLasPersonas)
+      .then((resp) => resp);
+
+    return valoresPorBarrio.map((x) => {
+      return {
+        barrio: x.barrio,
+        positivos: x.positivos,
+      };
+    });
+
+  }
 }
